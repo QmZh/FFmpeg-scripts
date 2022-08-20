26467a00e0dc517fb44d1ed1d5b02c21c11a04fc
Qimeng ZHANG <zhangqimeng98@163.com>
2022-08-20 20:23:38 +0800

    remove duplicated audio volume adjust

--- vid-or-aud-audio-volume-adjust.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-if "%~2"=="" (echo "parameter list: input file, audio multiplies by, audio bitrate, output file(optional, defaults to *-audio_times-*.*)" & echo "it could be used on videos too" & exit) 
-if "%~3"=="" (SET audio_bitrate=192k) else (SET audio_bitrate=%3)
-if "%~4"=="" (SET output_name="%~n1-audio_times-%2%~x1") else (SET output_name="%~n4%~x1")
-ffmpeg -hide_banner -hwaccel cuda -i %1 -c:v copy -filter:a "volume=%2" -b:a %audio_bitrate% %output_name%
